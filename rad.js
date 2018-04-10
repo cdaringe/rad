73da6f415a96da17ba61684b5af446d976f0a458
cdaringe <christopher.dieringer@gmail.com>
2018-04-10 09:14:33 -0700

    docs(pkg): target => output

--- rad.js
+++ rad.js
@@ -1,17 +1,17 @@
 module.exports = {
   tasks: {
     yarn: {
-      target: 'package.json',
+      input: 'package.json',
       cmd: 'yarn'
     },
     build: {
-      target: 'bundle.zip',
+      output: 'bundle.zip',
       dependsOn: ['yarn'],
       cmd: opts => `
-        zip ${opts.task.target} \\
+        zip ${opts.task.output} \\
           src \\
           node_modules \\
-          ${opts.upstream.yarn.task.target} # i.e package json
+          ${opts.upstream.yarn.task.input}
       `
     }
   }
