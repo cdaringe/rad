947c0a698556eb31611218aa5a9513cc0f694fb9
cdaringe <cdaringe@gmail.com>
2020-12-05 19:12:22 -0800

    refactor: improve integration test to use local refs

--- test/integration/rad.cli.init.sh
+++ test/integration/rad.cli.init.sh
@@ -5,4 +5,13 @@ mkdir -p /test-rad-project
 cd /test-rad-project
 export RAD_IMPORT_URL=/radness/src/mod.ts
 rad -l info --init
+
+# update rad file during integration test to full from local install, vs http
+echo [debug]: integration test radfile:
+cat rad.ts
+echo [debug] Replacing 'import type { Task, Tasks } from SOME_HTTP_URL' with local import
+rad_import_from_local='import type { Task, Tasks } from "/radness/src/mod.ts";'
+sed -i "1s|.*|$rad_import_from_local|" rad.ts
+
+# run integration task task
 rad greet
