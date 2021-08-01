324af487607ab561ed49db7a1e0866586ccbeda3
cdaringe <cdaringe@gmail.com>
2021-08-01 12:37:41 -0700

    chore: fix site animations

--- assets/site/common.ts
+++ assets/site/common.ts
@@ -1,5 +1,8 @@
 export type Point = [number, number];
-export type Transform = (i: TransformInput) => RadSvgTransform[];
+export type Transform = {
+  name: string;
+  fn: (i: TransformInput) => RadSvgTransform[];
+};
 export type TransformInput = { w: number; h: number; count: number };
 export type RadSvgTransform = {
   translate: [number, number];
