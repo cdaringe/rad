0d79612724f553b9a91804c1ff720daa57295e14
cdaringe <cdaringe@users.noreply.github.com>
2022-03-20 20:57:17 +0000

    deploy: 46a84bf3c949bc07b07dddcb1b1ae00155f63b57

--- transforms.js
+++ transforms.js
@@ -1,19 +1,21 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
 const TWO_PI = 2 * Math.PI;
 const radToDeg = (rad)=>rad / TWO_PI * 360
 ;
 const transform = {
     name: "orbit",
-    fn: ({ w , h , count ,  })=>{
+    fn: ({ w , h , count: count1 ,  })=>{
         const [cx, cy] = [
             w / 2,
             h / 2
         ];
         const xforms = [];
         const outer_ring_r = 0.6 * cx;
-        const inner_ring_r = 2;
-        const ring_count = 5;
         const ring_spacing_r = (outer_ring_r - 2) / (5 - 1);
-        const times = (count1)=>"_".repeat(count1).split("")
+        const times = (count)=>"_".repeat(count).split("")
         ;
         const ring_radii = times(5).map((_, i)=>2 + i * ring_spacing_r
         );
@@ -23,7 +25,7 @@ const transform = {
             sum_circumferences += c;
             return c;
         });
-        const count_by_ring_index = ring_circumferences.map((c, _)=>Math.floor(count * c / sum_circumferences)
+        const count_by_ring_index = ring_circumferences.map((c, _)=>Math.floor(count1 * c / sum_circumferences)
         );
         const radians_intervals_by_ring_index = count_by_ring_index.map((num, _)=>2 * Math.PI / num
         );
@@ -50,8 +52,8 @@ const transform = {
         return xforms;
     }
 };
-const transforms1 = [
+const transforms = [
     transform,
     transform, 
 ];
-export { transforms1 as transforms };
+export { transforms as transforms };
