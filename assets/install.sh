da3adaccf9814614049ecbef33e868e393694dd5
cdaringe <cdaringe@gmail.com>
2024-09-17 13:41:20 -0700

    feat: drop --unstable

--- assets/install.sh
+++ assets/install.sh
@@ -2,4 +2,4 @@
 set -exo pipefail
 curl -fsSL https://deno.land/x/install/install.sh | sh
 export __RAD_VERSION__=6.7.0
-deno install --unstable -f -A -n rad https://deno.land/x/rad@v$__RAD_VERSION__/src/bin.ts
+deno install -f -A -n rad https://deno.land/x/rad@v$__RAD_VERSION__/src/bin.ts
--- test/integration/rad.cli.init.sh
+++ test/integration/rad.cli.init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -ex
-deno install --unstable -f -A -n rad $PWD/src/bin.ts
+deno install -f -A -n rad $PWD/src/bin.ts
 mkdir -p /test-rad-project
 cd /test-rad-project
 export RAD_IMPORT_URL=/radness/src/mod.ts
